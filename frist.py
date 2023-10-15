d7147629dbf331b54b1ec38721d463a83ecb9d51
Ravidu321 <147970564+Ravidu321@users.noreply.github.com>
2023-10-15 13:56:09 +0530

    frist.py

--- frist.py
+++ frist.py
@@ -1,10 +1,37 @@
-def remove_comments(input_file, output_file):
-    with open(input_file, 'r') as infile:
-        with open(output_file, 'w') as outfile:
-            for line in infile:
-                if not line.strip().startswith('#'):
-                    outfile.write(line)
+# Function to add two numbers
+def add(x, y):
+    return x + y
 
-input_file = 'your_python_code.py'
-output_file = 'code_without_comments.py'
-remove_comments(input_file, output_file)
+# Function to subtract two numbers
+def subtract(x, y):
+    return x - y
+
+# Function to multiply two numbers
+def multiply(x, y):
+    return x * y
+
+# Function to divide two numbers
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+# User input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+# Perform the selected operation
+if operation == '+':
+    result = add(num1, num2)
+elif operation == '-':
+    result = subtract(num1, num2)
+elif operation == '*':
+    result = multiply(num1, num2)
+elif operation == '/':
+    result = divide(num1, num2)
+else:
+    result = "Invalid operation"
+
+# Display the result
+print(f"Result: {result}")
